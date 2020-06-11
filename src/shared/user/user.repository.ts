3bd53427cbc463ed9fbc520825e199640cd5b55c
Anthony Gorode <anthony.gorode@gmail.com>
2020-06-11 03:08:25 +0200

    add doc in userRepository

--- src/shared/user/user.repository.ts
+++ src/shared/user/user.repository.ts
@@ -2,9 +2,24 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 
+/**
+ * Provide all apis to select and manage users
+ *
+ * @export
+ * @class UserRepository
+ * @extends {Repository<User>}
+ */
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     
+    /**
+     * Select and join a specific user with
+     * his notifications & billing and delivery address
+     *
+     * @param {number} id_user user id
+     * @returns {Promise<User>} return the user details
+     * @memberof UserRepository
+     */
     public selectUserById(id_user: number): Promise<User> {
         return this.findOne({
             join: {
